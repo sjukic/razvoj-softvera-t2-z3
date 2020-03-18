41ab1c37939c2d25adfc982052fa529ce38343e0
Your Name <sjukic1@etf.unsa.ba>
2020-03-18 11:30:52 +0100

    Najduza rijec se pretvara u velika slova

--- src/ba/unsa/etf/rs/tut2zad3/Controller.java
+++ src/ba/unsa/etf/rs/tut2zad3/Controller.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rs.tut2zad3;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextArea ispisRecenice;
+    public TextField unosRecenice;
+
+    public void malaUVelika(ActionEvent actionEvent) {
+        String recenica = new String();
+        recenica = unosRecenice.getText();
+        String najduza = new String();
+        for (String rijec: recenica.split(" ")) {
+            if (rijec.length() > najduza.length())
+                najduza = rijec;
+        }
+        recenica = recenica.replace(najduza, najduza.toUpperCase());
+        System.out.println(recenica);
+        ispisRecenice.setText(recenica);
+    }
 }
